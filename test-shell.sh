d12719c30097c3ee361e60b1803211563d972535
Whisperity <whisperity@gmail.com>
2021-07-13 21:11:05 +0200

    Symlink ~/Dotfiles in the test shell to ~/Dotfiles of the original home

--- test-shell.sh
+++ test-shell.sh
@@ -7,7 +7,13 @@ TEMPHOME=$(mktemp -d)
 pushd ${TEMPHOME}
 touch .is_dotfiles_temporary_home \
     .sudo_as_admin_successful       # Don't show the Ubuntu default message...
+
+# Set up ~/Dotfiles in the temporary home as a symbolic link to the hardcoded
+# automatically used ~/Dotfiles "source repository" of the script.
+ln -s $(readlink -f ~/Dotfiles) ./Dotfiles
+
 popd
+
 
 clear
 HOME="${TEMPHOME}" bash
